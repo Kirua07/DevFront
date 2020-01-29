19bb0a2711d5b769cd9612d2fcb61ea9ca130fc4
Kirua07 <Kirua07@users.noreply.github.com>
2020-01-29 12:22:00 +0100

    Update monjs.js

--- monjs.js
+++ monjs.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   //  $('h1').hide();
 
   for ( index = 0; index < 5; index++) {
-      $('<div class="col-1">').appendto('#effets');
+      $('<div class="col-1">').appendTo('#effets');
       
   }
 });
