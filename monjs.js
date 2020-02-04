f51e77c6ee2015c92757431f1d616a5918c627e1
Kirua07 <Kirua07@users.noreply.github.com>
2020-02-04 09:26:23 +0100

    maj

--- monjs.js
+++ monjs.js
@@ -4,5 +4,10 @@ $(document).ready(function(){
   for ( index = 0; index < 5; index++) {
       $('<div class="col-1">').appendTo('#effets');
       }
- 
+$('#show').click(function(){
+  $function('#effets div').hide(2000);
+});
+$("#effets div").click(function(){
+  $( this ).hide(2000);
+});
 });
